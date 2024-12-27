7d151c3c9999ef3d27db8b7f7193e906bfd444d5
srxrt <str9703@gmail.com>
2024-12-27 10:51:37 +0900

    feat: integrate meta tags on document private file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,20 @@ export default function Document() {
   console.log("DOCUMENT - PAGES ROUTER");
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="title" content="Nestar Demo" />
+        <meta name="robots" content="index,follow" />
+        <link rel="icon" type="image/png" href="/img/logo/favicon.svg" />
+        <meta
+          name="keyword"
+          content="nestar, nestar.uz, devex, mern, mern nestjs fullstack"
+        />
+        <meta
+          name="description"
+          content="Buy and sell properties anywhere anytime in South Korea"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
