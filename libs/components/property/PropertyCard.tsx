25b16f5c5d57e05c3177d2d38cdb67b9482afeda
srxrt <str9703@gmail.com>
2024-12-31 15:09:43 +0900

    feat: publish propertyList component

--- libs/components/property/PropertyCard.tsx
+++ libs/components/property/PropertyCard.tsx
@@ -1,5 +1,83 @@
-import { Stack } from "@mui/material";
+import { Favorite, RemoveRedEye } from "@mui/icons-material";
+import { Box, IconButton, Stack, Typography } from "@mui/material";
+import Link from "next/link";
 
 const PropertyCard = () => {
-  return <Stack>PropertyCard</Stack>;
+  const imagePath: string = "/img/banner/header1.svg";
+  return (
+    <Stack className="card-config">
+      <Stack className="top">
+        <Link
+          href={{
+            pathname: "/property/detail",
+            query: { id: "test123dsjkfaj" },
+          }}
+        >
+          <img src={imagePath} alt="" />
+        </Link>
+        <Box className="top-badge">
+          <img src="/img/icons/electricity.svg" alt="" />
+          <Typography>TOP</Typography>
+        </Box>
+        <Box className="price-box">
+          <Typography>$250000</Typography>
+        </Box>
+      </Stack>
+
+      <Stack className="bottom">
+        <Stack className="name-address">
+          <Stack className="name">
+            <Link
+              href={{
+                pathname: "/property/detail",
+                query: { id: "test123dsjkfaj" },
+              }}
+            >
+              <Typography>Kingstone Apartments</Typography>
+            </Link>
+          </Stack>
+          <Stack className="address">
+            <Typography>Busan, City Center 200-5</Typography>
+          </Stack>
+        </Stack>
+        <Stack className="options">
+          <Stack className="option">
+            <img src="/img/icons/bed.svg" alt="" />
+            <Typography>2 bed</Typography>
+          </Stack>
+          <Stack className="option">
+            <img src="/img/icons/room.svg" alt="" />
+            <Typography>7 room</Typography>
+          </Stack>
+          <Stack className="option">
+            <img src="/img/icons/expand.svg" alt="" />
+            <Typography>170 m2</Typography>
+          </Stack>
+        </Stack>
+        <Stack className="divider"></Stack>
+
+        <Stack className="type-buttons">
+          <Stack className="type">
+            <Typography sx={{ fontWeight: 500, fontSize: "13px" }}>
+              Rent
+            </Typography>
+            <Typography sx={{ fontWeight: 500, fontSize: "13px" }}>
+              Barter
+            </Typography>
+          </Stack>
+          <Stack className="buttons">
+            <IconButton color="default">
+              <RemoveRedEye />
+            </IconButton>
+            <Typography className="view-cnt">100</Typography>
+            <IconButton color="default">
+              <Favorite color="primary" />
+            </IconButton>
+            <Typography className="view-cnt">20</Typography>
+          </Stack>
+        </Stack>
+      </Stack>
+    </Stack>
+  );
 };
+export default PropertyCard;
--- pages/property/index.tsx
+++ pages/property/index.tsx
@@ -1,10 +1,13 @@
 import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
 import Filter from "@/libs/components/property/Filter";
+import PropertyCard from "@/libs/components/property/PropertyCard";
 import { KeyboardArrowDownRounded } from "@mui/icons-material";
-import { Box, Button, Container, Stack } from "@mui/material";
+import { Box, Button, Pagination, Stack, Typography } from "@mui/material";
 import { NextPage } from "next";
+import { useState } from "react";
 
 const PropertyList: NextPage = () => {
+  const [properties, setProperties] = useState<number[]>([1, 2, 3, 4, 5, 6]);
   return (
     <div id="property-list-page" style={{ position: "relative" }}>
       <Stack className="container">
@@ -18,7 +21,27 @@ const PropertyList: NextPage = () => {
           <Stack className="filter-config">
             <Filter />
           </Stack>
-          <Stack className="main-config"></Stack>
+          <Stack className="main-config" mb={"76px"}>
+            <Stack className="list-config">
+              {properties.map((property, index) => {
+                return <PropertyCard key={index} />;
+              })}
+            </Stack>
+
+            <Stack className="pagination-config">
+              <Stack className="pagination-box">
+                <Pagination
+                  page={1}
+                  count={5}
+                  shape="circular"
+                  color="primary"
+                />
+              </Stack>
+              <Stack className="total-result">
+                <Typography>Total 5 properties available</Typography>
+              </Stack>
+            </Stack>
+          </Stack>
         </Stack>
       </Stack>
     </div>
